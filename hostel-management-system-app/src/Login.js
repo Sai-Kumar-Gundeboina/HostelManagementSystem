04a2cb7e4e5944551599776c6ae65670dcd3cf94
saikumar8639 <Gundeboinasaikumar86@gmail.com>
2025-06-07 12:30:19 +0530

    addition of login component

---
+++ hostel-management-system-app/src/Login.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+export default function Login() {
+    const[form, setForm]=useState({"username":"","password":""})
+    const handleChange=(e)=>{
+        setForm({...form, [e.target.name]:e.target.value})
+    }
+    const handleLogin=(e)=>{
+        e.preventDefault();
+        alert(`ENtered ${form.username} - ${form.password}`);
+        setForm({"username":"","password":""});
+    }
+  return (
+    <div>
+        <h1>Login</h1>
+        <form onSubmit={handleLogin} method='POST'>
+            <label>Username: </label>
+            <input 
+                onChange={handleChange}
+                name='username'
+                type="text" 
+                placeholder='username'
+                value={form.username} required/><br/>
+            <label>Password: </label>
+            <input 
+                onChange={handleChange}
+                name="password"
+                type="password" 
+                placeholder='password'
+                value={form.password} required/><br/>
+            <button type='submit'>Submit</button>
+        </form>
+    </div>
+  )
+}
